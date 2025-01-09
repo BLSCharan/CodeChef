09e88bf9d836a637469e587c1ebdbe118a4b52b9
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-01-09 23:13:16 +0530

    Update Mahasena.py

--- 500-1000 difficulty/Mahasena.py
+++ 500-1000 difficulty/Mahasena.py
@@ -1,1 +1,16 @@
+def is_ready_for_battle():
+    n = int(input())
+    
+    weapons = list(map(int, input().split()))
+    
+    even_count = sum(1 for weapon in weapons if weapon % 2 == 0)
+    odd_count = n - even_count  
+    
+    if even_count > odd_count:
+        print("READY FOR BATTLE")
+    else:
+        print("NOT READY")
+
+
+is_ready_for_battle()
 
