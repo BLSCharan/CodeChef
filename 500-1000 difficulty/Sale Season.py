45a25500e3b2513d31fcb9ebc34af201bbc98a9a
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-01-12 19:52:50 +0530

    Update Sale Season.py

--- 500-1000 difficulty/Sale Season.py
+++ 500-1000 difficulty/Sale Season.py
@@ -1,1 +1,13 @@
-
+t=int(input())
+while(1<=t<=100):
+    x=int(input())
+    if x<=100:
+        print(x)
+    elif(100<x<=1000):
+        print(x-25)
+    elif(1000<x<=5000):
+        print(x-100)
+    else:
+        print(x-500)
+        
+    t-=1
