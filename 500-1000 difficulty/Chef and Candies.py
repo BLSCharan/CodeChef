5a8e4015747bc5d9161ce59c4ed8623b5fad5eca
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-01-16 21:04:52 +0530

    Update Chef and Candies.py

--- 500-1000 difficulty/Chef and Candies.py
+++ 500-1000 difficulty/Chef and Candies.py
@@ -1,1 +1,10 @@
-
+from math import ceil
+t=int(input())
+for _ in range(t):
+    x,y=map(int,input().split())
+    z=x-y
+    a=z/4
+    if x<y:
+        print(0)
+    else:
+        print(ceil(a))
