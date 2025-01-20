ae368ef7f3cb0470530d8abda3203b599eac8d0c
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-01-20 20:13:41 +0530

    Update Problems in your to-do list.py

--- 500-1000 difficulty/Problems in your to-do list.py
+++ 500-1000 difficulty/Problems in your to-do list.py
@@ -1,1 +1,7 @@
+T = int(input())
+for _ in range(T):
+    N = int(input())
+    difficulties = list(map(int, input().split()))
+    to_remove = sum(1 for d in difficulties if d >= 1000)
+    print(to_remove)
 
