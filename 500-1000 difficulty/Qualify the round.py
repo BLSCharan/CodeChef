b74c1900d17b2bf1e406b92f78b3db7fd6319fa4
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-01-21 18:44:08 +0530

    Update Qualify the round.py

--- 500-1000 difficulty/Qualify the round.py
+++ 500-1000 difficulty/Qualify the round.py
@@ -1,1 +1,11 @@
-
+t=int(input())
+while(1<=t):
+    x,a,b=map(int,input().split())
+    c=a*1
+    d=b*2
+    e=c+d
+    if e>=x:
+        print("Qualify")
+    else:
+        print("NotQualify")
+    t-=1
