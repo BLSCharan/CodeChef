8e2f76e934cfca1d2a75f2ecbf9ef2ea25fce2bc
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-01-21 18:26:18 +0530

    Update Sasta Shark Tank.py

--- 500-1000 difficulty/Sasta Shark Tank.py
+++ 500-1000 difficulty/Sasta Shark Tank.py
@@ -1,1 +1,11 @@
-
+t=int(input())
+for _ in range(t):
+    a,b=map(int,input().split())
+    x=a*10
+    y=b*5
+    if x>y:
+        print("FIRST")
+    elif y>x:
+        print("SECOND")
+    else:
+        print("ANY")
