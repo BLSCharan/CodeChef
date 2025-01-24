78dbd0c756c13e6f678d4fa68b5489fb55d9d24a
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-01-24 19:15:10 +0530

    Update Bus Seat Numbering.py

--- 500-1000 difficulty/Bus Seat Numbering.py
+++ 500-1000 difficulty/Bus Seat Numbering.py
@@ -1,1 +1,12 @@
-
+t=int(input())
+while(1<=t):
+    n=int(input())
+    if n<=10:
+        print("Lower Double")
+    elif 10<n<=15:
+        print("Lower Single")
+    elif 15<n<=25:
+        print("Upper Double")
+    else:
+        print("Upper Single")
+    t-=1
