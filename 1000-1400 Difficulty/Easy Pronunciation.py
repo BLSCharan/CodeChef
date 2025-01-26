9b2ec00d4d79010c25c351b9a34a9804010cb600
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-01-26 22:00:19 +0530

    Update Easy Pronunciation.py

--- 1000-1400 Difficulty/Easy Pronunciation.py
+++ 1000-1400 Difficulty/Easy Pronunciation.py
@@ -1,1 +1,20 @@
-
+t=int(input())
+v={'a','e','i','o','u'}
+while(1<=t):
+    hard=False
+    c=0
+    n=int(input())
+    s=input().strip()
+    for i in s:
+        if i not in v:
+            c+=1
+            if c>=4:
+                hard=True
+                break
+        else:
+            c=0
+    if hard:
+        print("NO")
+    else:
+        print("YES")
+    t-=1
