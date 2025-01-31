26d67a6bfd786201b1cbf43a18c49bb85514310f
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-01-31 21:10:29 +0530

    Update Mario and Transformation.py

--- 500-1000 difficulty/Mario and Transformation.py
+++ 500-1000 difficulty/Mario and Transformation.py
@@ -1,1 +1,10 @@
-
+t=int(input())
+while(1<=t):
+    x=int(input())
+    if x%3==2:
+        print("SMALL")
+    elif x%3==0:
+        print("NORMAL")
+    elif x%3==1:
+        print("HUGE")
+    t-=1
