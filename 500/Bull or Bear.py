296ff8f948b14d995f42590ca3e1530e70ddb457
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-01 22:49:57 +0530

    Update Bull or Bear.py

--- 500/Bull or Bear.py
+++ 500/Bull or Bear.py
@@ -1,1 +1,9 @@
-
+t=int(input())
+for _ in range(t):
+    x,y=map(int,input().split())
+    if x>y:
+        print("LOSS")
+    elif x==y:
+        print("NEUTRAL")
+    else:
+        print("PROFIT")
