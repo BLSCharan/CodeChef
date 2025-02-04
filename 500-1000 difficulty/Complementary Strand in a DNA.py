0bbc2d8d324b27dd8d786c8f595c605bcc331f95
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-04 21:48:44 +0530

    Update Complementary Strand in a DNA.py

--- 500-1000 difficulty/Complementary Strand in a DNA.py
+++ 500-1000 difficulty/Complementary Strand in a DNA.py
@@ -1,1 +1,13 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())  
+    s = input().strip()  
+    complement = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+
+    result = ""
+    for c in s:
+        result += complement[c]
+
+    print(result)
 
