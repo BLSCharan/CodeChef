ef1c44f0b5f293d07691e8f48b5831d6c7af582a
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-08 22:48:07 +0530

    Update Candy Distribution.py

--- 500-1000 difficulty/Candy Distribution.py
+++ 500-1000 difficulty/Candy Distribution.py
@@ -1,1 +1,13 @@
+t=int(input())
+while 1<=t:
+    x,y=map(int,input().split())
+    if x%y==0:
+        z=x//y
+        if z%2==0:
+            print("Yes")
+        else:
+            print("No")
+    else:
+        print("No")
+    t-=1
 
