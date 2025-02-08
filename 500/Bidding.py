e51ef5268649e67f0d6581875a5fe5fa70d9837b
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-08 22:20:12 +0530

    Update Bidding.py

--- 500/Bidding.py
+++ 500/Bidding.py
@@ -1,1 +1,10 @@
-
+t=int(input())
+while(1<=t):
+    a,b,c=map(int,input().split())
+    if a>b and a>c:
+        print("Alice")
+    elif b>a and b>c:
+        print("Bob")
+    else:
+        print("Charlie")
+    t-=1
