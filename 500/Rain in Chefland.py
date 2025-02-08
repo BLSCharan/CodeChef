c2da86b47a0aaace08780f3b8078e0a617dfe20e
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-08 19:52:55 +0530

    Update Rain in Chefland.py

--- 500/Rain in Chefland.py
+++ 500/Rain in Chefland.py
@@ -1,1 +1,10 @@
-
+t=int(input())
+while(1<=t):
+    x=int(input())
+    if x<3:
+        print("LIGHT")
+    elif 3<=x<7:
+        print("MODERATE")
+    elif x>=7:
+        print("HEAVY")
+    t-=1
