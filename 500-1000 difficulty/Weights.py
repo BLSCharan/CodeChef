ed285f956ba9a2da56c2c129286d7abed89fdd2b
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-14 19:47:06 +0530

    Update Weights.py

--- 500-1000 difficulty/Weights.py
+++ 500-1000 difficulty/Weights.py
@@ -1,1 +1,7 @@
-
+t = int(input()) 
+for _ in range(t):
+    w,x,y,z= map(int, input().split())  
+    if w==(x+y) or w==(y+z) or w==(x+z) or w==(x+y+z) or w == x or w == y or w == z:
+        print("YES")
+    else:
+        print("NO")
