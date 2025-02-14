2f225d76a22e6d7bf94785ff9900463a745a861f
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-14 19:37:19 +0530

    Update Water Mixing.py

--- 500-1000 difficulty/Water Mixing.py
+++ 500-1000 difficulty/Water Mixing.py
@@ -1,1 +1,14 @@
+t = int(input()) 
+for _ in range(t):
+    a, b, x, y = map(int, input().split())  
+    if b >= a:  
+        if b - a <= x:  
+            print("YES")
+        else:
+            print("NO")
+    else:
+        if a - b <= y:  
+            print("YES")
+        else:
+            print("NO")
 
