1ae5d43ea7a41c3d22c25678266274eb7f28e503
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-16 22:44:10 +0530

    Update Chef and his Apps.py

--- 500-1000 difficulty/Chef and his Apps.py
+++ 500-1000 difficulty/Chef and his Apps.py
@@ -1,1 +1,9 @@
-
+for i in range(int(input())):
+    s,x,y,z=map(int,input().split())
+    n=s-x-y
+    if n>=z:
+        print("0")
+    elif (s-min(x,y)>=z):
+        print("1")
+    else:
+        print("2")
