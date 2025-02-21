75412d7a668c97c11630cc9d17a113508afc6352
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-21 19:16:32 +0530

    Update Speed Limit Test.py

--- 500-1000 difficulty/Speed Limit Test.py
+++ 500-1000 difficulty/Speed Limit Test.py
@@ -1,1 +1,11 @@
-
+t=int(input())
+for _ in range(t):
+    a,x,b,y=map(int,input().split())
+    p=a/x
+    q=b/y
+    if q>p:
+        print("Bob")
+    elif p>q:
+        print("Alice")
+    else:
+        print("Equal")
