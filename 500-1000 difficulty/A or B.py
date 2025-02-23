30ad5747445d4f3127c9dd479b9d84cdebe36cea
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-23 21:21:32 +0530

    Update A or B.py

--- 500-1000 difficulty/A or B.py
+++ 500-1000 difficulty/A or B.py
@@ -1,1 +1,10 @@
-
+t=int(input())
+for _ in range(t):
+    a,b=map(int,input().split())
+    c=500-a*2
+    d=1000-(a+b)*4
+    e=c+d
+    f=1000-b*4
+    g=500-(a+b)*2
+    h=f+g
+    print(max(e,h))
