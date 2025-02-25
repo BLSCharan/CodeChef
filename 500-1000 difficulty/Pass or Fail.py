520d52f6256ed476b4bf296996d210db9bb29fcc
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-25 18:46:38 +0530

    Update Pass or Fail.py

--- 500-1000 difficulty/Pass or Fail.py
+++ 500-1000 difficulty/Pass or Fail.py
@@ -1,1 +1,13 @@
-
+t=int(input())
+for _ in range(t):
+    n,x,p=map(int,input().split())
+    a=n-x
+    b=a*(-1)
+    c=x*3
+    d=(b+c)
+    if d>=p:
+        print("PASS")
+    else:
+        print("FAIL")
+    
+    
