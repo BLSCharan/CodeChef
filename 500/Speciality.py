4408422f5d16f73cd0dfb698db1efd22be51a254
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-25 18:31:27 +0530

    Update Speciality.py

--- 500/Speciality.py
+++ 500/Speciality.py
@@ -1,1 +1,9 @@
-
+t=int(input())
+for _ in range(t):
+    a,b,c=map(int,input().split())
+    if a>b and a>c:
+        print("Setter")
+    elif b>c and b>a:
+        print("Tester")
+    else:
+        print("Editorialist")
