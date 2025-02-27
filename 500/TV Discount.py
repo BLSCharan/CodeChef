c6b6d42ff8cc30bfd292638d1adc11e463e17f51
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-27 19:16:00 +0530

    Update TV Discount.py

--- 500/TV Discount.py
+++ 500/TV Discount.py
@@ -1,1 +1,11 @@
-
+t=int(input())
+for _ in range(t):
+    a,b,c,d=map(int,input().split())
+    x=a-c
+    y=b-d
+    if x<y:
+        print("First")
+    elif x>y:
+        print("Second")
+    else:
+        print("Any")
