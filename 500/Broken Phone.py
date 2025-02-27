11df483dfa6a559239197df9ec8b5a36de84c638
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-02-27 19:19:22 +0530

    Update Broken Phone.py

--- 500/Broken Phone.py
+++ 500/Broken Phone.py
@@ -1,1 +1,9 @@
-
+t=int(input())
+for _ in range(t):
+    a,b=map(int,input().split())
+    if a<b:
+        print("REPAIR")
+    elif a>b:
+        print("NEW PHONE")
+    else:
+        print("ANY")
