e0c805b1c834684bf9b6fe9ff774fc0ae1dd4bff
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-15 22:03:13 +0530

    Update Chef and Happy String.py

--- 1000-1400 Difficulty/Chef and Happy String.py
+++ 1000-1400 Difficulty/Chef and Happy String.py
@@ -1,1 +1,18 @@
+t = int(input())
+vowels = {"a", "e", "i", "o", "u"}
+
+for _ in range(t):
+    s = input().strip().lower()
+    count = 0
+
+    for char in s:
+        if char in vowels:
+            count += 1
+            if count > 2:
+                print("Happy")
+                break
+        else:
+            count = 0 
+    else:
+        print("Sad")
 
