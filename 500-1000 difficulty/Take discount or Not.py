d89201427f89ba5dd1628e75eb9e6510ded85177
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-19 19:04:30 +0530

    Update Take discount or Not.py

--- 500-1000 difficulty/Take discount or Not.py
+++ 500-1000 difficulty/Take discount or Not.py
@@ -1,1 +1,17 @@
+t = int(input())
+
+for _ in range(t):
+    n, x, y = map(int, input().split())
+    a = list(map(int, input().split()))
+    b=sum(a)
+    c=[]
+    for i in range(len(a)):
+        c.append(max(0,a[i]-y))
+    d=sum(c)
+    e=d+x
+    if e<b:
+        print("COUPON")
+    else:
+        print("NO COUPON")
+  
 
