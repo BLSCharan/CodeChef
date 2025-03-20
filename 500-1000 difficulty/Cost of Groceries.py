5c02d83c7a61a3343e195ceeb9a86e28d44a2c65
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-20 09:36:43 +0530

    Update Cost of Groceries.py

--- 500-1000 difficulty/Cost of Groceries.py
+++ 500-1000 difficulty/Cost of Groceries.py
@@ -1,1 +1,15 @@
+t = int(input())
+
+for _ in range(t):
+    n, x = map(int, input().split())
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+    t=0
+    for i in range(n):
+        if a[i]>=x:
+            t+=b[i]
+    print(t)
+            
+            
+    
 
