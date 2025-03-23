01b91ce1db016307234826187880afd396c2d26f
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-23 21:49:37 +0530

    Update Largest and Second Largest.py

--- 500-1000 difficulty/Largest and Second Largest.py
+++ 500-1000 difficulty/Largest and Second Largest.py
@@ -1,1 +1,10 @@
+t = int(input())
 
+while t > 0:
+    n = int(input())
+    a = list(map(int, input().split()))
+    b = sorted(list(set(a)))
+    if len(b) >= 2:
+        c = b[len(b) - 1] + b[len(b) - 2]
+        print(c)
+    t -= 1
