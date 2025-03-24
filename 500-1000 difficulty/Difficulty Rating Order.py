09a75039da6b0bba117978d8ec1aa4722d70aaf6
Behara Lakshmi Sai Charan <blscharan2004@gmail.com>
2025-03-24 21:50:12 +0530

    Update Difficulty Rating Order.py

--- 500-1000 difficulty/Difficulty Rating Order.py
+++ 500-1000 difficulty/Difficulty Rating Order.py
@@ -1,1 +1,13 @@
+t = int(input())  
+
+for _ in range(t):
+    n = int(input()) 
+    d = list(map(int, input().split())) 
+
+    for i in range(1, n):
+        if d[i] < d[i - 1]:
+            print("No")
+            break
+    else:
+        print("Yes")
 
